f1c0cd1abb2cfb28db20579972dc26f232aa074b
David Gardiner <david@gardiner.net.au>
2022-06-18 14:11:20 +0930

    Initial terraform files

---
+++ main.tf
@@ -0,0 +1,24 @@
+terraform {
+  required_providers {
+    azurerm = {
+      source  = "hashicorp/azurerm"
+      version = "~> 3.10.0"
+    }
+  }
+  backend "azurerm" {
+      resource_group_name  = "rg-tfdemo-state-australiaeast"
+      storage_account_name = "sttfdemostateausteast"
+      container_name       = "tfstate"
+      key                  = "terraform.tfstate"
+  }
+  required_version = ">= 1.1.0"
+}
+
+provider "azurerm" {
+  features {}
+}
+
+resource "azurerm_resource_group" "state-demo-secure" {
+  name     = "rg-tfdemo-australiaeast"
+  location = "australiaeast"
+}
