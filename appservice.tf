4ec22a30ca7fcaadd8dc3d7cbdb547375a973fa0
David Gardiner <david@gardiner.net.au>
2022-06-19 20:59:28 +0930

    F1 and SystemAssigned identity

--- appservice.tf
+++ appservice.tf
@@ -3,7 +3,7 @@ resource "azurerm_service_plan" "plan" {
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   os_type             = "Linux"
-  sku_name            = "P1v2"
+  sku_name            = "F1"
 }
 
 resource "azurerm_linux_web_app" "app" {
@@ -23,5 +23,9 @@ resource "azurerm_linux_web_app" "app" {
     name  = "Database"
     type  = "SQLAzure"
     value = "Server=${azurerm_mssql_server.mssql.fully_qualified_domain_name};Database=${azurerm_mssql_database.database.name};User ID=${var.mssql_administrator_login};Password=${var.mssql_administrator_password};Application Name=App Service"
+  }
+
+  identity {
+    type = "SystemAssigned"
   }
 }
