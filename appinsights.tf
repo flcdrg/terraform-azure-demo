cb7629250b4ec318bfda1ef077000a97aa7d9074
David Gardiner <david@gardiner.net.au>
2022-06-27 22:42:28 +0930

    Don't need outputs

--- appinsights.tf
+++ appinsights.tf
@@ -4,11 +4,3 @@ resource "azurerm_application_insights" "appinsights" {
   location            = azurerm_resource_group.rg.location
   application_type    = "web"
 }
-
-output "instrumentation_connection_string" {
-  value = azurerm_application_insights.appinsights.connection_string
-}
-
-output "app_id" {
-  value = azurerm_application_insights.appinsights.app_id
-}
