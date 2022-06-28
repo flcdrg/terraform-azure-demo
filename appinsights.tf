2dc5fb2fcbd5ab3df96c0c15e0a9076ab361eb9c
David Gardiner <david@gardiner.net.au>
2022-06-28 18:16:15 +0930

    add log analytics

--- appinsights.tf
+++ appinsights.tf
@@ -1,6 +1,14 @@
+resource "azurerm_log_analytics_workspace" "la" {
+  name                = "log-tfdemo-australiaeast"
+  resource_group_name = azurerm_resource_group.rg.name
+  location            = azurerm_resource_group.rg.location
+  sku                 = "Free"
+}
+
 resource "azurerm_application_insights" "appinsights" {
   name                = "appi-tfdemo-australiaeast"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
   application_type    = "web"
+  workspace_id        = azurerm_log_analytics_workspace.la.id
 }
