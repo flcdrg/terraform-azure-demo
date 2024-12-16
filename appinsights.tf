1d4717312e6064b519c3b93bfc59d916c357297f
David Gardiner <david@gardiner.net.au>
2024-12-16 16:59:13 +1030

    Update Log Analytics Workspace SKU

--- appinsights.tf
+++ appinsights.tf
@@ -2,7 +2,7 @@ resource "azurerm_log_analytics_workspace" "la" {
   name                = "log-tfdemo-australiaeast"
   resource_group_name = azurerm_resource_group.rg.name
   location            = azurerm_resource_group.rg.location
-  sku                 = "Free"
+  sku                 = "PerGB2018"
 }
 
 resource "azurerm_application_insights" "appinsights" {
